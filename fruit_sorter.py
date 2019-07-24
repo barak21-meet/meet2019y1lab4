c91ed00a01fae948f916bf6824eebe44782a7019
barak21-meet <barak21@meet.mit.edu>
2019-07-24 14:27:17 +0300

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,9 @@
+f=input("what fruit is it?")
+if f=='apple':
+    print('apples in bin 1')
+elif f=='orange':
+    print('oranges in binn 2')
+elif f=='banana':
+    print('bananas in bin 3')
+else:
+    print('Error: I do not recognize that fruit')
